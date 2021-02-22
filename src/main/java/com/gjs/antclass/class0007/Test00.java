2407dfffbf432e7ba990cd9b96c7782b9e74fd91
gujiashun <781010964@qq.com>
2021-02-22 15:11:52 +0800

    【+】class0007

---
+++ src/main/java/com/gjs/antclass/class0007/Test00.java
@@ -0,0 +1,17 @@
+package com.gjs.antclass.class0007;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Test00
+ *
+ * @author gujiashun
+ * @date 2021/2/11
+ */
+public class Test00 {
+
+    public static void main(String[] args) {
+//        new Thread(new Test01(), "input thread name").start();
+        new Test02().start();
+    }
+}
---
+++ src/main/java/com/gjs/antclass/class0007/Test02.java
@@ -0,0 +1,45 @@
+package com.gjs.antclass.class0007;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Test02
+ *
+ * @author gujiashun
+ * @date 2021/2/11
+ */
+public class Test02 extends Thread {
+
+    Lock lock = new ReentrantLock();
+
+
+    @Override
+    public void run() {
+        set();
+    }
+
+    public void set() {
+        try {
+            lock.lock();
+            System.out.println("set");
+            get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void get() {
+        try {
+            lock.lock();
+            System.out.println("进入get");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
